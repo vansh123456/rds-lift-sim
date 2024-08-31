ebf6270c19207786746c4505cfe50e914a3d8e3b
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-08-31 19:24:38 +0530

    js: introduce queue

--- js/main.js
+++ js/main.js
@@ -6,3 +6,37 @@ let leftDoors = document.querySelectorAll(".left-door");
 let rightDoors = document.querySelectorAll(".right-door");
 const floorsContainer = document.querySelector(".floors");
 let floors = document.querySelectorAll(".floor");
+
+//to perform basic operations we make use of queue DS,we add at the back and remove from the front 
+//we make use of prebuilt enqueue and dequeue
+
+class Queue {
+    constructor() {
+        this.items = []; //define an empty array to add elements into
+      }
+      enqueue(element) {
+        return this.items.push(element); // add element in array
+      }
+    
+      dequeue() {
+        if (this.items.length > 0) { //check if queue is empty
+          return this.items.shift();  //nuke items
+        }
+      }
+    
+      peek() {
+        return this.items[this.items.length - 1]; //display the element
+      }
+    
+      isEmpty() {
+        return this.items.length == 0;
+      }
+    
+      size() {
+        return this.items.length;
+      }
+    
+      clear() {
+        this.items = [];
+      }
+}
