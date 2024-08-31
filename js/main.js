0b307d7c73268fa15b882836c92fc4aa8f12fb35
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-08-31 20:01:55 +0530

    add functions to enque or dequeue elements

--- js/main.js
+++ js/main.js
@@ -94,6 +94,9 @@ const lifts = Array.from(document.querySelectorAll(".lift-container"), (el) => (
       moveLift(lift.htmlEl, requests.dequeue(), index);
     }
   };
+  /**
+   * Lift animations defined hereon now
+   */
   const openLift = (index) => {
     buttons.disabled = true;
     rightDoors[index].classList.add("right-door-open");
@@ -123,7 +126,6 @@ const lifts = Array.from(document.querySelectorAll(".lift-container"), (el) => (
       closeLift(index);
     }, 3000);
   };
-  
   const moveLift = (lift, destFloor, index) => {
     const distance = Math.abs(destFloor - lifts[index].currFloor);
     lift.style.transform = `translateY(${destFloor * 100 * -1}%)`;
@@ -135,15 +137,6 @@ const lifts = Array.from(document.querySelectorAll(".lift-container"), (el) => (
   
     lifts[index].currFloor = destFloor;
   };
-  
-  /**
-   *
-   *    REQUESTS:
-   *    addRequest
-   *    removeRequest
-   *
-   */
-  
   function addRequest(destFloor) {
     requests.enqueue(destFloor);
     dispatchRequestAdded();
@@ -152,127 +145,3 @@ const lifts = Array.from(document.querySelectorAll(".lift-container"), (el) => (
   function removeRequest() {
     requests.dequeue();
   }
-  
-  /**
-   *
-   *      EVENTS:
-   *      requestAddedEvent
-   *      liftIdleEvent
-   *
-   */
-  
-  const requestAddedEvent = new Event("requestAdded");
-  const liftIdleEvent = new Event("liftIdle");
-  
-  function dispatchRequestAdded() {
-    document.dispatchEvent(requestAddedEvent);
-  }
-  
-  function dispatchliftIdle() {
-    document.dispatchEvent(liftIdleEvent);
-  }
-  
-  document.addEventListener("requestAdded", () => {
-    callLift();
-  });
-  
-  document.addEventListener("liftIdle", () => {
-    if (!requests.isEmpty()) {
-      callLift();
-    }
-  });
-  
-  /**
-   *
-   *    ADD DOM Elements
-   *    - addLift
-   *    - addFloor
-   *
-   */
-  
-  function addLift() {
-    floors[floors.length - 1].append(getLiftEl());
-    liftEls = document.querySelectorAll(".lift-container");
-    lifts.push({
-      htmlEl: liftEls[liftEls.length - 1],
-      busy: false,
-      currFloor: 0,
-    });
-    leftDoors = document.querySelectorAll(".left-door");
-    rightDoors = document.querySelectorAll(".right-door");
-  
-    if(lifts.length >= getMaxLifts()) {
-      console.log("Max lifts added");
-      addLiftBtn.disabled = true;
-      addLiftBtn.textContent = "Max lifts added"
-      return;
-    }
-  }
-  
-  function getLiftEl() {
-    const liftDistance = (lifts.length + 1) * 120;
-  
-    const liftEL = document.createElement("div");
-    liftEL.classList.add("lift-container");
-    liftEL.style.position = "absolute";
-    liftEL.style.left = `${liftDistance}px`;
-  
-    liftEL.innerHTML += `
-              <div class="left-door">
-              </div>
-              <div class="right-door">
-              </div>
-          `;
-  
-    return liftEL;
-  }
-  
-  function addFloor() {
-    floorsContainer.prepend(getFloorEl());
-    floors = document.querySelectorAll(".floor");
-    buttons = document.querySelectorAll(".call-lift-btn");
-    addCallLiftListeners([buttons[0], buttons[1]]);
-  }
-  
-  function getFloorEl() {
-    const newLiftNum = floors.length;
-  
-    const floorEl = document.createElement("div");
-    floorEl.classList.add("floor");
-    floorEl.innerHTML += `
-                    <div class="lift-buttons">
-                        <button class="call-lift-btn open-lift-btn" data-lift-num="${newLiftNum}">
-                            <i class="fa-solid fa-angle-up"></i>
-                        </button>
-                        <button class="call-lift-btn close-lift-btn" data-lift-num="${newLiftNum}">
-                            <i class="fa-solid fa-angle-down"></i>
-                        </button>
-                    </div>
-                  `;
-    return floorEl;
-  }
-  
-  function addCallLiftListeners(buttons) {
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener("click", () => {
-        addRequest(buttons[i].dataset.liftNum);
-      });
-    }
-  }
-  
-  /**
-   *
-   * MAIN
-   *
-   */
-  
-  let requests = new Queue();
-  
-  function main() {
-    addCallLiftListeners(buttons);
-    addFloorBtn.addEventListener("click", addFloor);
-    addLiftBtn.addEventListener("click", addLift);
-  }
-  
-  main();
-  
