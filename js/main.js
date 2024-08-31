48ff6f386eb46afba3ae09024e0f368fcb3e59ab
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-08-31 19:48:48 +0530

    add lift functions

--- js/main.js
+++ js/main.js
@@ -46,3 +46,51 @@ const lifts = Array.from(document.querySelectorAll(".lift-container"), (el) => (
     busy: false,
     currFloor: 0,
   }));
+  function getLifts() {
+    return lifts;
+  }
+  
+  function getClosestEmptyLift(destFloor) {
+    const lifts = getLifts();
+  
+    const emptyLifts = lifts.reduce(
+      (result, value, i) =>
+        result.concat(
+            //here used ternary operators
+          value.busy === false
+            ? {
+                i,
+                currFloor: value.currFloor,
+                distance: Math.abs(destFloor - value.currFloor),//get the nearest value in absolute whole digits
+              }
+            : []
+        ),
+      []
+    );
+  
+    if (emptyLifts.length <= 0) {
+      return { lift: {}, index: -1 };
+    }
+  
+    const closestLift = emptyLifts.reduce((result, value, index) =>
+      value.distance < result.distance ? value : result
+    );
+  
+    const index = closestLift.i;
+  
+    return { lift: lifts[index], index };
+  }
+  const getMaxLifts = () => {
+    const viewportwidth = document.getElementsByTagName("body")[0].clientWidth;
+    return Math.floor(((viewportwidth - 100)/120));
+  };
+  
+  const callLift = () => {
+    //destructured values added 
+    const { lift, index } = getClosestEmptyLift(requests.peek());
+  
+    if (index >= 0) {
+      lifts[index].busy = true;
+      moveLift(lift.htmlEl, requests.dequeue(), index);
+    }
+  };
