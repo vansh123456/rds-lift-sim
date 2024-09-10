3d537228f45cf72ab2a6d262e1f0cdb9602ea769
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-09-10 19:16:25 +0530

    js: add guard logic for input

--- js/main.js
+++ js/main.js
@@ -3,3 +3,29 @@ const floorsContainer = document.querySelector('.floors-container');
 const mainContainer = document.querySelector('.main-container');
 const lifts = [];
 const pendingQueue = [];
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    const numFloorsInput = document.getElementById('floors');
+    const numLiftsInput = document.getElementById('lifts');
+    const numFloors = Number(numFloorsInput.value);
+    const numLifts = Number(numLiftsInput.value);
+  
+    if (!numLifts || !numFloors || numFloors < 1 || numLifts < 1) {
+      alert(
+        'Please enter valid values for both number of floors and number of lifts.'
+      );
+      return;
+    }
+    if (numLifts > numFloors) {
+        alert(
+          'Number of lifts should be less than or equal to the number of floors.'
+        );
+        return;
+      }
+    
+      mainContainer.style.display = 'none';
+      generateFloors(numFloors);
+      generateLifts(numLifts);
+    });
