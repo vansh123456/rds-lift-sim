e062f6559fbcc1f15b98c602cb7ae7750c464fa0
vansh123456 <vanshbhatnagar14dev@gmail.com>
2024-09-10 19:14:12 +0530

    initial js file

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('.form');
+const floorsContainer = document.querySelector('.floors-container');
+const mainContainer = document.querySelector('.main-container');
+const lifts = [];
+const pendingQueue = [];
